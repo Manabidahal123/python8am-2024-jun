fec4f613032546175bbbfc3c5f7d55e44c5031c5
Manabidahal123 <dahalmanabi142gmail.com>
2024-06-20 09:30:29 +0545

    first commit

---
+++ main.py
@@ -0,0 +1,52 @@
+# # print('hello world')
+
+# # data=[
+
+# #     [12,34,56,76],
+# #     [45,[[600],700],67,78,89]
+# # ]
+# # print(data[1][1][1])
+
+# # data=(34,56,76)
+# # data=list(data)
+# # data[0]=900
+
+
+# data={3,4,5,3,4}
+# # print(data)
+# # (set)
+
+# data={
+#     'name':'ram',
+#     'age':23,
+#     'address':'kathmandu',
+#     'contact':{
+#         'email':'ram@gmail.com',
+#         'phone':{
+#             'home':123456,
+#             'office':123456
+
+#         }
+#     }
+# }
+
+# print(data['contact']['phone']['office'])
+
+
+# data={
+#      'name':'ram',
+#      'age':23,
+#      'address':'kathmandu'
+
+# }
+
+# # print(data['names'])
+# print(data.get('name','key not found'))
+
+# # print('we are learning dictionary')
+
+# # print(data.keys())
+# print(data.values())
+
+
+
